d9dc94a347c57376de50e95a65d4a8ef7770c340
yplopez <yuney.lopez@iesnervion.es>
2024-03-05 14:37:22 +0100

    primer commit

---
+++ src/Ej1/CuentaCorriente.java
@@ -0,0 +1,70 @@
+package Ej1;
+
+public class CuentaCorriente {
+
+	private String dni;
+	private String nombre;
+	private double saldo;
+
+	public CuentaCorriente(String dni, int saldo) {
+		if (nombre != null && !nombre.equals(""))
+		{
+			this.dni = dni;
+		}
+		if (saldo > 0) {
+			this.saldo = saldo;
+		}
+	}
+
+	public CuentaCorriente(String dni, String nombre, int saldo){
+		this (dni, saldo);		
+		if (nombre != null && !nombre.equals("")) {			
+		this.nombre = nombre;
+		}		
+	}
+	
+
+	public String getDni() {
+		return dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		if (nombre != null && !nombre.equals("")) {
+		this.nombre = nombre;
+		}
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public boolean sacarDinero(double dinero) {
+		boolean opRealizada = false;
+
+		if (this.saldo >= dinero) {
+			this.saldo -= dinero;
+			opRealizada = true;		
+		}
+		return opRealizada;
+	}
+
+	public void ingresarDinero(double dinero) {
+		
+		if (dinero > 0) {
+			this.saldo += dinero;
+		}
+	}
+	
+	public void mostrarInformacion () {
+		System.out.println("***********************\n");
+		System.out.println("Titular: " + this.nombre + "\n");
+		System.out.println("DNI: " + this.dni + "\n");
+		System.out.println("Saldo: " + this.saldo + "\n");
+		System.out.println("***********************");
+	}
+
+}
---
+++ src/Ej1/Principal.java
@@ -0,0 +1,16 @@
+package Ej1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		CuentaCorriente c1 = new CuentaCorriente ("12345678A", "Nomb Apell", 100);
+		
+		c1.ingresarDinero(200);
+		c1.mostrarInformacion();
+		
+		c1.sacarDinero(50);
+		c1.mostrarInformacion();
+
+	}
+
+}
